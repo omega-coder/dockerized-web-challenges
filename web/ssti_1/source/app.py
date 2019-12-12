4e090fb4ebee5e7a152c7c9d09c55b7e1d357aef
CHERIEF Yassine <fy_cherief@esi.dz>
2019-12-12 01:43:36 +0100

    Adds source for ssti_1

---
+++ web/ssti_1/source/app.py
@@ -0,0 +1,31 @@
+
+
+import os
+from flask import Flask, render_template, render_template_string, request
+
+
+app = Flask(__name__)
+app.secret_key = "shellmates{REDACTED}"
+
+app.config["LEVEL"] = os.environ.get('SSTI_LEVEL')
+
+if app.config["LEVEL"] is not None:
+    template_name = "level_{}.html".format(app.config['LEVEL'])
+else:
+    template_name = "level_1.html"
+
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+
+    if request.method == "GET":
+        rendered_template = render_template(template_name, exploit=None)
+        return render_template_string(rendered_template), 200
+
+    else:
+        exploit = request.form['exploit']
+        rendered_template = render_template(template_name, exploit=exploit)
+        return render_template_string(rendered_template), 200
+
+if __name__ == "__main__":
+    app.run(port=9000, host='0.0.0.0')
