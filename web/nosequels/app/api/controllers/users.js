6277ba05a6e265aecfa40899891407d7c1d09c6e
CHERIEF Yassine <fy_cherief@esi.dz>
2019-12-15 15:05:16 +0100

    Check if new user is named admin

--- web/nosequels/app/api/controllers/users.js
+++ web/nosequels/app/api/controllers/users.js
@@ -3,20 +3,28 @@ const jwt = require('jsonwebtoken');
 
 module.exports = {
     'create': function (req, res, next) {
-        userModel.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        }, function (err, result) {
-            if (err)
-                next(err);
-            else
-                res.json({
-                    status: "success",
-                    message: "User added successfully!",
-                    data: null
-                });
-        })
+        if (req.body.name === 'admin') {
+            res.json({
+                status: "error",
+                message: "you can't create a user with the admin name"
+            });
+        } else {
+            userModel.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            }, function (err, result) {
+                if (err)
+                    next(err);
+                else
+                    res.json({
+                        status: "success",
+                        message: "User added successfully!",
+                        data: null
+                    });
+            });
+        }
+
     },
     'authenticate': function (req, res, next) {
         userModel.findOne({
@@ -28,10 +36,11 @@ module.exports = {
             } else {
                 const token = jwt.sign({
                     id: userInfo._id,
+                    name: userInfo.name,
                 }, req.app.get('secret_key'), {
                     expiresIn: '1h'
                 });
-
+                delete userInfo.password;
                 res.json({
                     status: "success",
                     message: "User Found!!",
