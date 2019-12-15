d0d695c61c7423f19d3e1818679177b2fda7b28f
CHERIEF Yassine <fy_cherief@esi.dz>
2019-12-15 05:11:15 +0100

    adds create and auth methods for user controller

---
+++ web/nosequels/app/api/controllers/users.js
@@ -0,0 +1,46 @@
+const userModel = require('../models/users');
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+    'create': function (req, res, next) {
+        userModel.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.email
+        }, function (err, result) {
+            if (err)
+                next(err);
+            else
+                res.json({
+                    status: "success",
+                    message: "User added successfully!",
+                    data: null
+                });
+        })
+    },
+    'authenticate': function (req, res, next) {
+        userModel.findOne({
+            name: req.body.name,
+            password: req.body.password
+        }, function (err, userInfo) {
+            if (err) {
+                next(err);
+            } else {
+                const token = jwt.sign({
+                    id: userInfo._id,
+                }, req.app.get('secret_key'), {
+                    expiresIn: '1h'
+                });
+
+                res.json({
+                    status: "success",
+                    message: "User Found!!",
+                    data: {
+                        user: userInfo,
+                        token: token
+                    }
+                });
+            }
+        })
+    }
+}
