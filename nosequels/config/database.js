22bd6c38daab0de66493d04a1dd1d7d178220cf2
CHERIEF Yassine <fy_cherief@esi.dz>
2019-12-20 21:48:41 +0100

    Get mongo creds from .env file

--- nosequels/config/database.js
+++ nosequels/config/database.js
@@ -1,5 +1,15 @@
-const mongoose = require('mongoose');
-const mongoDB = 'mongodb://localhost/nosql_challenge';
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const CHALLENGE_USERNAME = process.env.CHALLENGE_USERNAME;
+const CHALLENGE_DATABASE_NAME = process.env.CHALLENGE_DATABASE_NAME;
+const CHALLENGE_PASSWORD = process.env.CHALLENGE_PASSWORD;
+const CHALLENGE_DB_HOST = process.env.CHALLENGE_DB_HOST;
+
+const mongoDB = `mongodb+srv://${CHALLENGE_USERNAME}:${CHALLENGE_PASSWORD}@${CHALLENGE_DB_HOST}/${CHALLENGE_DATABASE_NAME}?retryWrites=true&w=majority`;
+
+console.log(mongoDB);
 
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
